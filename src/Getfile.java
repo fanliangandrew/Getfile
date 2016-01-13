88c7b56ea93c06b65343917c00a2c44f7e926e13
fanliangandrew <986406153@qq.com>
2016-01-14 01:17:29 +0800

    First commit of the Getfile java File.

---
+++ src/Getfile.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class Getfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		String b,c;
+		b="D:\\HCBA";
+		c="\\Testfile";  //divide the path into several parts
+		File file=new File(b+c);
+		if(!file.exists()){
+			a=0;
+			System.out.println(a+"File donot exi\n");
+			file.mkdirs();
+		}
+		else {
+			a=1;
+			System.out.println(a+"Has it\n");}
+	}
+
+}
